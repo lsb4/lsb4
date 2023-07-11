e0a311e97dbb24852f4385f04202856a8462d09d
lsb4 <lsb4@cin.ufpe.br>
2023-07-10 23:03:22 -0300

    aboutMe texts added

--- src/components/AboutMe/aboutMe.tsx
+++ src/components/AboutMe/aboutMe.tsx
@@ -19,40 +19,38 @@ export const AboutMe: React.FC<AboutMeProps> = () => {
       />
       <div className="flex flex-col-reverse md:flex-row justify-center items-start gap-10 w-full max-w-[75%] text-white text-left mt-14">
         <div>
-          <div className="flex flex-col gap-4">
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </p>
+          <div className="flex flex-col">
             <p className="mb-2">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+              I am a Front-end developer with one year of professional
+              experience and fully capable of creating pages/interfaces that are
+              attractive and responsive. Throughout my career, I had the
+              opportunity to learn and improve myself in technologies such as
+              HTML, CSS, JavaScript, ReactJS, TypeScript, Tailwind CSS and
+              Styled Components.
             </p>
-            <p css={experience} className="text-[25px] font-medium">
+            <p className="mb-4">
+              Besides having a strong understanding of the fundamental
+              principles of front-end development, I also have an ease of
+              learning and adaptation, I am able to communicate clearly and
+              concisely with a team and, if necessary, lead them on a project.
+            </p>
+            <p css={experience} className="text-[25px] font-medium mb-4">
               Professional Experience
             </p>
-            <p className="text-[20px] font-medium">V-Lab ( 2022 - Present )</p>
-            <p className="mb-4">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+            <p className="text-[20px] font-medium mb-2">V-Lab ( 2022 - Present )</p>
+            <p className="mb-2">
+              As a front-end developer, I participate in a project in
+              partnership with the MEC (Ministério da Educação), where my main
+              assignment is the creation of responsive pages, using the latest
+              technologies and stacks. The pages created are destined to online
+              courses, with the objective of professionalizing and specializing
+              several types of workers.
+            </p>
+            <p className="mb-6">
+              Although the project is very front-end focused, many times other
+              challenges appear where I have to venture into other areas, such
+              as Python scripting, process optimization, image and web page
+              loading optimization, back-end techniques for API usage, etc.
             </p>
             <div className="flex gap-6">
               <a href={curriculo} download="curriculo.pdf" css={introButton}>
